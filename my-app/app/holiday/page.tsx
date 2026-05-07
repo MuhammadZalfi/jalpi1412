812e094a94edde35c77a45a3ebb04ff82a0e8c05
jalpi1412 <2413047@students.universitasmulia.ac.id>
2026-05-07 11:54:17 +0800

    quiz

---
+++ my-app/app/holiday/page.tsx
@@ -0,0 +1,37 @@
+export default function holiday(){
+    return (
+    <div className="bg-gray-200 h-screen flex gap-10 justify-center items-center">
+        <div className="bg-white h-screen h-140 w-70 rounded-4xl">
+            <div className="flex p-3 justify-center">
+                <img src="/jembatan.jpg" className= "w-75 mt-2.5 rounded 3xl "></img>
+            </div>
+            <div className ="flex flex-col">
+                <div></div> 
+                <h1 className="text-2xl font-bold text-black">San Francisco</h1>
+            </div>
+            <p>Premium Economy</p>
+        
+        <div/>
+
+             <div className="flex gap-4 text-black">
+               <div className="flex ap-2">
+                <img src="/tag.svg"/>
+                <p>from $240</p>
+            </div>
+            <div className="flex gap-4 text-black">
+            <div className="flex gap-2">
+                <img src="/plane-tilt.svg"/>
+                <p>sfo</p>
+            </div>
+        </div>
+    
+        </div>
+    
+        </div>
+        
+    
+    
+    </div>
+         
+    )
+}
